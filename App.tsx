10a72bd39c0683d115125052a37518784099a4da
sk97kang <sk97.kang@gmail.com>
2021-04-14 19:41:17 +0900

    Add AppIcon, Splash

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { ThemeProvider } from "@emotion/react";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import { client, isLoggedInVar } from "./common/apollo";
 import { AuthNav } from "./navigators/AuthNav";
 import { MainNav } from "./navigators/MainNav";
 import { theme } from "./styles/theme";
+import AppLoading from "expo-app-loading";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+
+  const preload = async () => {};
+  const onFinish = () => setLoading(false);
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+
   return (
     <ThemeProvider theme={theme.toki}>
       <ApolloProvider client={client}>
