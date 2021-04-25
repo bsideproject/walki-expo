4d4e7b124fe5bd7f78c71301042f5225c51c4206
choon <chachooon@gmail.com>
2021-04-25 18:18:07 +0900

    feat:  코치선택 카드 화면, 선택값 AsyncStorage 저장

--- components/Auth/CoachContainer.tsx
+++ components/Auth/CoachContainer.tsx
@@ -1,16 +1,41 @@
 import React, { useCallback, useState } from "react";
-import { Text, Dimensions } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import Carousel, { Pagination } from "react-native-snap-carousel";
 import styled from "@emotion/native";
 import { Button } from "../../components/Button";
-import { SafeLayout } from "../../layouts/SafeLayout";
+import { SvgXml } from "react-native-svg";
+import { images } from "../../styles/images";
 import { INaviProps } from "../../navigators/AuthStackNavi";
+import AsyncStorage from "@react-native-community/async-storage";
 
 const { width: screenWidth, height: screenHight } = Dimensions.get("window");
 
-const COACH_DATA = [
-  require("../../assets/images/card_01.png"),
-  require("../../assets/images/card_02.png"),
+interface ICoachData {
+  title: string;
+  subTitle: string;
+  source: any;
+  style: any;
+}
+
+const COACH_DATA: ICoachData[] = [
+  {
+    title: "나는 러닝이랑 도넛을\n사랑하는 토키야! ",
+    subTitle: "나랑 같이 동네 한 바퀴 어때?",
+    source: <SvgXml xml={images.coach_toki} />,
+    style: {
+      width: screenWidth,
+      height: screenHight / 2,
+    },
+  },
+  {
+    title: "나는 산책이랑 커피를\n사랑하는 부키야! ",
+    subTitle: "나랑 천천히 공원산책 해볼래?",
+    source: <SvgXml xml={images.coach_buki} />,
+    style: {
+      width: screenWidth,
+      height: screenHight / 2,
+    },
+  },
 ];
 
 /**
@@ -20,9 +45,62 @@ const COACH_DATA = [
 const CoachContainer = ({ goNext }: INaviProps) => {
   const [selectedCoachIndex, setSelectedCoachIndex] = useState(0);
 
-  const _renderItem = useCallback(({ item }: { item: number }) => {
-    return <Iamge source={item} />;
-  }, []);
+  const _renderItem = useCallback(
+    ({ item, index }: { item: ICoachData; index: number }) => {
+      return (
+        <CardView
+          style={{
+            borderStyle: "solid",
+            borderWidth: 3,
+            borderColor:
+              selectedCoachIndex === index
+                ? selectedCoachIndex < 1
+                  ? "#F06692"
+                  : "#8AD10A"
+                : "#FFF",
+          }}
+        >
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              height: "70%",
+            }}
+          >
+            {item.source}
+          </View>
+
+          <Text
+            numberOfLines={2}
+            adjustsFontSizeToFit
+            style={{
+              textAlign: "center",
+              fontWeight: "700",
+              fontSize: 18,
+              lineHeight: 27,
+              marginBottom: 4,
+            }}
+          >
+            {item.title}
+          </Text>
+          <Text
+            numberOfLines={1}
+            adjustsFontSizeToFit
+            style={{
+              textAlign: "center",
+              fontWeight: "400",
+              fontSize: 14,
+              lineHeight: 20,
+              marginBottom: 70,
+            }}
+          >
+            {item.subTitle}
+          </Text>
+        </CardView>
+      );
+    },
+    [selectedCoachIndex]
+  );
 
   const onPressSelect = useCallback(() => {
     // TODO: 코치선택 데이터 저장
@@ -66,26 +144,26 @@ const CoachContainer = ({ goNext }: INaviProps) => {
           data={COACH_DATA}
           renderItem={_renderItem}
           sliderHeight={screenHight}
-          sliderWidth={screenWidth - 32}
-          itemWidth={264 + 12}
-          itemHeight={200}
+          sliderWidth={screenWidth}
+          itemWidth={290}
+          activeSlideAlignment={"center"}
+          inactiveSlideScale={1}
           onSnapToItem={(index) => setSelectedCoachIndex(index)}
           slideStyle={{
             justifyContent: "center",
             alignItems: "center",
           }}
         />
-        <Pagination
-          dotsLength={2}
-          activeDotIndex={selectedCoachIndex}
-          inactiveDotOpacity={0.4}
-          inactiveDotScale={0.6}
-        />
         <BottomContainer>
           <Button
-            text="선택하기"
+            text="다음"
             onPress={() => {
-              if (typeof goNext === "function") goNext();
+              AsyncStorage.setItem(
+                "coach",
+                selectedCoachIndex < 1 ? "TOKI" : "BUKI"
+              ).then(() => {
+                if (typeof goNext === "function") goNext();
+              });
             }}
             type="secondary"
           />
@@ -109,4 +187,10 @@ const BottomContainer = styled.View`
   z-index: 100;
 `;
 
-const Iamge = styled.Image``;
+const CardView = styled.View`
+  width: 264px;
+  height: 360px;
+  background: #ffffff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.08);
+  border-radius: 16px;
+`;
