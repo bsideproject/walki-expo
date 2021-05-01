613fad9579a2e5f810a1e45bb7b9f949dd221981
choon <chachooon@gmail.com>
2021-05-01 23:01:14 +0900

    fix: 앱아이콘, 스플래쉬화면 설정 수정

--- App.tsx
+++ App.tsx
@@ -1,26 +1,28 @@
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { NavigationContainer } from "@react-navigation/native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
 import { client, isLoggedInVar } from "./common/apollo";
-import { Platform, StatusBar } from "react-native";
 import { MainStackNav } from "./navigators/MainStackNav";
 import { AuthStackNavi } from "./navigators/AuthStackNavi";
 import AsyncStorage from "@react-native-community/async-storage";
 import { ThemeLayout } from "./layouts/ThemeLayout";
 
 const App = () => {
-  const [isMember, setMemberCheck] = useState(false);
   const isLoggedIn = useReactiveVar(isLoggedInVar);
 
   useEffect(() => {
-    AsyncStorage.getItem("token").then((value) => setMemberCheck(true));
+    // TODO: 토큰 만료된 케이스 ?
+    AsyncStorage.getItem("token").then((value) => {
+      // isLoggedInVar(true);
+    });
   }, []);
 
   return (
     <ApolloProvider client={client}>
       <ThemeLayout>
         <NavigationContainer>
-          <StatusBar hidden={Platform.OS === "android"} />
+          <StatusBar style="dark" />
           {isLoggedIn ? <MainStackNav /> : <AuthStackNavi />}
         </NavigationContainer>
       </ThemeLayout>
